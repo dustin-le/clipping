b75ae3da6ef0a1e801f12487f175ca407a5f3239
Dustin Le <ledustin2@gmail.com>
2019-10-31 21:10:54 -0500

    Print out each line of each face.

--- hmwk_04.c
+++ hmwk_04.c
@@ -1,6 +1,6 @@
-// Dalio, Brian A.
-// dalioba
-// 2019-10-25
+// Le, Dustin E.
+// dxl0689
+// 2019-11-01
 //----------------------------------------------------------
 #include <stdio.h>
 #include <stdlib.h>
@@ -76,13 +76,49 @@ int main( int argc, char *argv[] )
 
   for ( int i=0; i<m->m_numFaces; i++ ) {
 
-    // TODO: Check each line of each face to see if
-    //       it should be drawn.  If so, draw it.
-    //       Use a Line structure to represent the
-    //       the line, the clipLine() routine to
-    //       clip it, and the dumpLine() routine to
-    //       "draw" it (if necessary).
+    Line line1, line2, line3;
+    double v1pX = m->m_vertex[m->m_face[i].v1].x;
+    double v1pY = m->m_vertex[m->m_face[i].v1].y;
+    double v2pX = m->m_vertex[m->m_face[i].v2].x;
+    double v2pY = m->m_vertex[m->m_face[i].v2].y;
+    double v3pX = m->m_vertex[m->m_face[i].v3].x;
+    double v3pY = m->m_vertex[m->m_face[i].v3].y;
 
+    // Line 1: Vertex 1 to Vertex 2
+    line1.p1X = v1pX;
+    line1.p1Y = v1pY;
+    line1.p2X = v2pX;
+    line1.p2Y = v2pY;
+
+    // Line 2: Vertex 2 to Vertex 3
+    line2.p1X = v2pX;
+    line2.p1Y = v2pY;
+    line2.p2X = v3pX;
+    line2.p2Y = v3pY;
+
+    // Line 3: Vertex 3 to Vertex 1
+    line3.p1X = v3pX;
+    line3.p1Y = v3pY;
+    line3.p2X = v1pX;
+    line3.p2Y = v1pY;
+
+    int test1 = clipLine(v, &line1);
+    if (test1)
+    {
+      dumpLine(&line1);
+    }
+
+    int test2 = clipLine(v, &line2);
+    if (test2)
+    {
+      dumpLine(&line2);
+    }
+
+    int test3 = clipLine(v, &line3);
+    if (test3)
+    {
+      dumpLine(&line3);
+    }
   }
 
   //--------------------------------------
